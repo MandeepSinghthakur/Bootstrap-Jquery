5dfd0dbe5bbf75a8bc2a3222ac6c2c844c926a7e
Mandeep <mandeep419.singh@gmail.com>
2016-01-06 20:04:59 -0600

    added VS soltion

---
+++ BootStrapJqueryProtek/main.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+   
+    //init function
+    function init() {
+        getdata();
+    }
+
+    // call init
+    init();
+
+
+
+});
+   
+// ajax function
+function getdata() {
+    $.ajax({
+        url: "data.json",
+        method: "GET",
+        success: function (resp) {
+            $.each(resp.HomeNav_Links, function (index, value) {
+                $(".navbar-nav").append(
+                    '<li><a>' + value + '</a></li>'
+                    );
+            })          
+        }
+    })
+
+}
